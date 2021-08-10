f20142ed67dc019eacfab6be03b327ceabf4e85c
Glitch (glitch-hello-website) <none>
2021-08-10 19:05:27 +0000

    🍗🛤 Checkpoint ./index.html:33818177/4627 ./project.html:33818177/8821 ./script.js:33818177/2168 ./style.css:33818177/3150

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
-
 function addPrices() {
+  var x = 0;
+  var y = 0;
+  var z = 0;
+  var priceTotal;
 
-var x = 0;
-var y = 0;
-var z = 0;
-var priceTotal;
+  var checkboxeschips = document.querySelector(
+    `input[value="Kettle Cooked Chips"]:checked`
+  );
+  var checkboxesramen = document.querySelector(
+    `input[value="Ramen Noodles"]:checked`
+  );
+  var checkboxescake = document.querySelector(
+    `input[value="Junior's Cheesecake"]:checked`
+  );
 
-var checkboxeschips = document.querySelector(`input[value="Kettle Cooked Chips"]:checked`);
-var checkboxesramen = document.querySelector(`input[value="Ramen Noodles"]:checked`);
-var checkboxescake = document.querySelector(`input[value="Junior's Cheesecake"]:checked`);
+  if (checkboxeschips) {
+    x = x + 3.32;
+  }
+  if (checkboxesramen) {
+    y = y + 0.5;
+  }
+  if (checkboxescake) {
+    z = z + 9.95;
+  }
 
-if (checkboxeschips) {
-  x = x+3.32;
-} 
-if (checkboxesramen){
-  y =  y+0.5;
+  priceTotal = x + y + z;
+
+  console.log(priceTotal);
+
+  document.getElementById("totalprice").innerHTML = priceTotal;
 }
-if (checkboxescake){
-  z = z+9.95;
-} 
-
-priceTotal = x+y+z;
-
-console.log(priceTotal);
-
-document.getElementById("totalprice").innerHTML = priceTotal;
-
-
-}
-
 
 function getSelectedCheckboxValues(name) {
   const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
   let values = [];
-  checkboxes.forEach((checkbox) => {
+  checkboxes.forEach(checkbox => {
     values.push(checkbox.value);
   });
   return values;
 }
 
-
 function checkboxes() {
-  
-  const btn = document.querySelector('#btn');
-  btn.addEventListener('click', (event) => {
-    alert("You Chose: " + getSelectedCheckboxValues('Foods') +  
-     " Click the picture to buy!");
+  const btn = document.querySelector("#btn");
+  btn.addEventListener("click", event => {
+    alert(
+      "You Chose: " +
+        getSelectedCheckboxValues("Foods") +
+        " Click the picture to buy!"
+    );
   });
 }
-
